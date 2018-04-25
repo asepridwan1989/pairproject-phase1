2457776aa9d8f4e25da2a50452097730598f1e5c
yasirmaulana <yasir.maulana@gmail.com>
2018-04-25 12:42:43 +0700

    fitur login - form only, dimerge dulu untuk disamakan routing nya

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 app.set('view engine', 'ejs')
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+// routing
+const register = require('./routes/register')
 const robot = require('./routes/robot')
+
+app.get('/', function(req,res){
+    res.render('index')
+})
+app.use('/register', register)
 app.use('/robot', robot)
 
 
