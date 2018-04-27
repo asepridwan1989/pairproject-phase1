2e4284a427aaf767e41b4528299ccdb94b2022fc
asepridwan1989 <aseprmail@gmail.com>
2018-04-27 08:56:34 +0700

    suka

---
+++ migrations/20180427002437-create-commentator.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Commentators', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      poster: {
+        type: Sequelize.STRING
+      },
+      MatchId: {
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Commentators');
+  }
+};
